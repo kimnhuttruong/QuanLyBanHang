c34ebf791f74d32943032128bc68c84151f09306
nguyentienthanh2321998 <50145697+nguyentienthanh2321998@users.noreply.github.com>
2019-12-09 13:36:01 +0700

    thanhga

---
+++ DoAnCK_TTA/DTO/DTO_STOCK_OUTWARD.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnCK_TTA.DTO
+{
+    public class DTO_STOCK_OUTWARD
+    {
+        string ID;
+        string RefStatus;
+        string PaymentMethod;      
+        string PaymentDate;
+        string DeliveryDate;
+        string Department_ID;
+        string Employee_ID;
+        string Stock_ID;
+        string Customer_ID;
+        string CustomerName;
+        string CustomerAddress;
+        string Contact;
+        string Payment;
+        string Currency_ID;
+        string ExchangeRate;
+        string Vat;
+        string VatAmount;
+        string Amount;
+        string FAmount;
+        string DiscountDate;
+        string DiscountRate;
+        string Discount;
+        string OtherDiscount;
+        string Charge;
+        string Cost;
+        string Profit;
+        string User_ID;
+        string IsClose;
+        string Sorted;
+        string Description;
+        bool Active;
+
+        public string ID1 { get => ID; set => ID = value; }
+        public string RefStatus1 { get => RefStatus; set => RefStatus = value; }
+        public string PaymentMethod1 { get => PaymentMethod; set => PaymentMethod = value; }
+        public string PaymentDate1 { get => PaymentDate; set => PaymentDate = value; }
+        public string DeliveryDate1 { get => DeliveryDate; set => DeliveryDate = value; }
+        public string Department_ID1 { get => Department_ID; set => Department_ID = value; }
+        public string Employee_ID1 { get => Employee_ID; set => Employee_ID = value; }
+        public string Stock_ID1 { get => Stock_ID; set => Stock_ID = value; }
+        public string Customer_ID1 { get => Customer_ID; set => Customer_ID = value; }
+        public string CustomerName1 { get => CustomerName; set => CustomerName = value; }
+        public string CustomerAddress1 { get => CustomerAddress; set => CustomerAddress = value; }
+        public string Contact1 { get => Contact; set => Contact = value; }
+        public string Payment1 { get => Payment; set => Payment = value; }
+        public string Currency_ID1 { get => Currency_ID; set => Currency_ID = value; }
+        public string ExchangeRate1 { get => ExchangeRate; set => ExchangeRate = value; }
+        public string Vat1 { get => Vat; set => Vat = value; }
+        public string VatAmount1 { get => VatAmount; set => VatAmount = value; }
+        public string Amount1 { get => Amount; set => Amount = value; }
+        public string FAmount1 { get => FAmount; set => FAmount = value; }
+        public string DiscountDate1 { get => DiscountDate; set => DiscountDate = value; }
+        public string DiscountRate1 { get => DiscountRate; set => DiscountRate = value; }
+        public string Discount1 { get => Discount; set => Discount = value; }
+        public string OtherDiscount1 { get => OtherDiscount; set => OtherDiscount = value; }
+        public string Charge1 { get => Charge; set => Charge = value; }
+        public string Cost1 { get => Cost; set => Cost = value; }
+        public string Profit1 { get => Profit; set => Profit = value; }
+        public string User_ID1 { get => User_ID; set => User_ID = value; }
+        public string IsClose1 { get => IsClose; set => IsClose = value; }
+        public string Sorted1 { get => Sorted; set => Sorted = value; }
+        public string Description1 { get => Description; set => Description = value; }
+        public bool Active1 { get => Active; set => Active = value; }
+    }
+}
