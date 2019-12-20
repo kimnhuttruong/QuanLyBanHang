5b27c9b45d9d5790b7009702a648fe3a3b1086e3
thanhtruongankhtn2019 <57841813+thanhtruongankhtn2019@users.noreply.github.com>
2019-12-20 20:08:04 +0700

    thanh gaaaa

---
+++ DoAnCK_TTA/GUI/frmDangKiBanQuyen.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace DoAnCK_TTA.GUI
+{
+    partial class frmDangKiBanQuyen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDangKiBanQuyen));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.simpleButton4 = new DevExpress.XtraEditors.SimpleButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtmamay = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMaDK = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMaKH = new System.Windows.Forms.TextBox();
+            this.ptb1 = new System.Windows.Forms.PictureBox();
+            this.ptb2 = new System.Windows.Forms.PictureBox();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.btnKetThuc = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb2)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(239)))), ((int)(((byte)(255)))));
+            this.groupBox1.Controls.Add(this.simpleButton3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Enabled = false;
+            this.groupBox1.Location = new System.Drawing.Point(-2, -13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(416, 60);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // simpleButton3
+            // 
+            this.simpleButton3.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton3.ImageOptions.Image")));
+            this.simpleButton3.Location = new System.Drawing.Point(14, 21);
+            this.simpleButton3.Name = "simpleButton3";
+            this.simpleButton3.PaintStyle = DevExpress.XtraEditors.Controls.PaintStyles.Light;
+            this.simpleButton3.Size = new System.Drawing.Size(36, 32);
+            this.simpleButton3.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label3.Location = new System.Drawing.Point(56, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(249, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Kích hoạt phần mềm quản lý Bán Hàng";
+            // 
+            // simpleButton4
+            // 
+            this.simpleButton4.Location = new System.Drawing.Point(313, 78);
+            this.simpleButton4.Name = "simpleButton4";
+            this.simpleButton4.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton4.TabIndex = 15;
+            this.simpleButton4.Text = "Gửi Mã Máy";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(45, 78);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Mã Máy";
+            // 
+            // txtmamay
+            // 
+            this.txtmamay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(239)))), ((int)(((byte)(255)))));
+            this.txtmamay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtmamay.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmamay.Location = new System.Drawing.Point(119, 79);
+            this.txtmamay.Name = "txtmamay";
+            this.txtmamay.Size = new System.Drawing.Size(179, 20);
+            this.txtmamay.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Mã đăng ký";
+            // 
+            // txtMaDK
+            // 
+            this.txtMaDK.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtMaDK.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaDK.Location = new System.Drawing.Point(119, 111);
+            this.txtMaDK.Name = "txtMaDK";
+            this.txtMaDK.Size = new System.Drawing.Size(269, 24);
+            this.txtMaDK.TabIndex = 18;
+            this.txtMaDK.TextChanged += new System.EventHandler(this.txtmk_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Mã kích hoạt";
+            // 
+            // txtMaKH
+            // 
+            this.txtMaKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaKH.Location = new System.Drawing.Point(119, 150);
+            this.txtMaKH.Name = "txtMaKH";
+            this.txtMaKH.Size = new System.Drawing.Size(269, 24);
+            this.txtMaKH.TabIndex = 20;
+            this.txtMaKH.TextChanged += new System.EventHandler(this.txtMaKH_TextChanged);
+            // 
+            // ptb1
+            // 
+            this.ptb1.Image = global::DoAnCK_TTA.Properties.Resources.cancel_32x32;
+            this.ptb1.Location = new System.Drawing.Point(365, 111);
+            this.ptb1.Name = "ptb1";
+            this.ptb1.Size = new System.Drawing.Size(23, 24);
+            this.ptb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ptb1.TabIndex = 22;
+            this.ptb1.TabStop = false;
+            this.ptb1.Visible = false;
+            // 
+            // ptb2
+            // 
+            this.ptb2.Image = global::DoAnCK_TTA.Properties.Resources.cancel_32x32;
+            this.ptb2.Location = new System.Drawing.Point(365, 150);
+            this.ptb2.Name = "ptb2";
+            this.ptb2.Size = new System.Drawing.Size(23, 24);
+            this.ptb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ptb2.TabIndex = 23;
+            this.ptb2.TabStop = false;
+            this.ptb2.Visible = false;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(311, 21);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton1.TabIndex = 13;
+            this.simpleButton1.Text = "Trợ Giúp";
+            // 
+            // btnKetThuc
+            // 
+            this.btnKetThuc.Location = new System.Drawing.Point(230, 21);
+            this.btnKetThuc.Name = "btnKetThuc";
+            this.btnKetThuc.Size = new System.Drawing.Size(75, 23);
+            this.btnKetThuc.TabIndex = 11;
+            this.btnKetThuc.Text = "Kết Thúc";
+            this.btnKetThuc.Click += new System.EventHandler(this.btnKetThuc_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(149, 21);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton2.TabIndex = 14;
+            this.simpleButton2.Text = "Gửi";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(239)))), ((int)(((byte)(255)))));
+            this.groupBox2.Controls.Add(this.simpleButton2);
+            this.groupBox2.Controls.Add(this.btnKetThuc);
+            this.groupBox2.Controls.Add(this.simpleButton1);
+            this.groupBox2.Location = new System.Drawing.Point(-2, 220);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(416, 59);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // frmDangKiBanQuyen
+            // 
+            this.Appearance.BackColor = System.Drawing.Color.White;
+            this.Appearance.Options.UseBackColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 278);
+            this.Controls.Add(this.ptb2);
+            this.Controls.Add(this.ptb1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMaKH);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMaDK);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtmamay);
+            this.Controls.Add(this.simpleButton4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmDangKiBanQuyen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Phần Mềm Quản Lý Bán Hàng";
+            this.Load += new System.EventHandler(this.frmDangKiBanQuyen_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb2)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtmamay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMaDK;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMaKH;
+        private System.Windows.Forms.PictureBox ptb1;
+        private System.Windows.Forms.PictureBox ptb2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+        private System.Windows.Forms.Label label3;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton btnKetThuc;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
