b63ac0ddfe91b5c266fec1d5ebb9c8cf463b97d3
an9x0010@gmail.com <an9x0010@gmail.com>
2019-12-20 19:54:37 +0700

    c

--- DoAnCK_TTA/DAL/DAL_SYS_USER.cs
+++ DoAnCK_TTA/DAL/DAL_SYS_USER.cs
@@ -4,15 +4,40 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DoAnCK_TTA.DAL
 {
     public class DAL_SYS_USER:DB_Connect
     {
-        public bool LayDanhSachPhanQuyen(string username, string password)
+        private string MaHoa(string text)
         {
+            //Tạo MD5 
+            MD5 mh = MD5.Create();
+            //Chuyển kiểu chuổi thành kiểu byte
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(text);
+            //mã hóa chuỗi đã chuyển
+            byte[] hash = mh.ComputeHash(inputBytes);
+            //tạo đối tượng StringBuilder (làm việc với kiểu dữ liệu lớn)
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+           
+            //nếu bạn muốn các chữ cái in thường thay vì in hoa thì bạn thay chữ "X" in hoa 
+            string pw = sb.ToString();
+            return sb.ToString();
+
+        }
+
+        public bool LayDanhSachPhanQuyen(string username,   string password)
+        {
+            password = MaHoa(password);
             DataTable dt = new DataTable();
             SqlDataAdapter da = new SqlDataAdapter();
             SqlCommand cmd = new SqlCommand();
@@ -57,7 +82,7 @@ namespace DoAnCK_TTA.DAL
             SqlCommand cmd = new SqlCommand();
             cmd.Connection = _conn;
             cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "SELECT UserName,Group_ID FROM dbo.SYS_USER";
+            cmd.CommandText = "SELECT UserName,Group_ID,Password FROM dbo.SYS_USER";
             try
             {
                 OpenConnection();
@@ -104,7 +129,7 @@ namespace DoAnCK_TTA.DAL
             SqlCommand cmd = new SqlCommand();
             cmd.Connection = _conn;
             cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "INSERT [dbo].[SYS_USER] VALUES ('" + ma + "', N'" + u.UserName + "', N'"+u.Password+"', N'" +u.Group_ID+"',N'',N'',"+ active + ",'"+u.Employee_ID+"')";
+            cmd.CommandText = "INSERT [dbo].[SYS_USER] VALUES ('" + ma + "', N'" + u.UserName + "', N'"+MaHoa(u.Password)+"', N'" +u.Group_ID+"',N'',N'',"+ active + ",'"+u.Employee_ID+"')";
             try
             {
                 OpenConnection();
@@ -126,6 +151,27 @@ namespace DoAnCK_TTA.DAL
             cmd.Connection = _conn;
             cmd.CommandType = CommandType.Text;
             cmd.CommandText = "  declare @nhom nvarchar(30) select @nhom = u.Group_ID from SYS_USER u where u.UserName = N'"+u+ "'  update FOrm set Description = @nhom    update FOrm set ENCaption = N'" + u + "'      update FOrm set Form_Caption = @@SERVERNAME  ";
+            try
+            {
+                OpenConnection();
+                cmd.ExecuteNonQuery();
+                CloseConnection();
+                return 1;
+            }
+            catch
+            {
+                return 0;
+            }
+
+        }
+        public int CapNhatMatKhau(string tk,string mkc,string mkm)
+        {
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = _conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "UPDATE dbo.SYS_USER SET Password ='" + MaHoa(mkm) + "' WHERE UserName = '" + tk + "' AND Password = '" + MaHoa(mkc) + "'";
             try
             {
                 OpenConnection();
--- DoAnCK_TTA/GUI/FrmDoiMatKhau.cs
+++ DoAnCK_TTA/GUI/FrmDoiMatKhau.cs
@@ -15,16 +15,19 @@ namespace DoAnCK_TTA.GUI
 {
     public partial class FrmDoiMatKhau : DevExpress.XtraEditors.XtraForm
     {
-        public FrmDoiMatKhau()
+        BUS_SYS_USER busform = new BUS_SYS_USER();
+        string id = "";
+        public FrmDoiMatKhau(string UserName)
         {
             InitializeComponent();
+            id = UserName;
         }
 
         private void simpleButton2_Click(object sender, EventArgs e)
         {
             this.Close();
         }
-
+      
         private void FrmDoiMatKhau_Load(object sender, EventArgs e)
         {
             BUS_SYS_LOG busLog = new BUS_SYS_LOG();
@@ -32,12 +35,23 @@ namespace DoAnCK_TTA.GUI
             BUS_SYS_USER busform = new BUS_SYS_USER();
             DataTable dtlog = new DataTable();
             dtlog = busform.LayThongTinUSER();
-            log.MChine = dtlog.Rows[0][1].ToString();
-            log.UserID = dtlog.Rows[0][2].ToString();
-            log.Module = this.Tag.ToString();
-            log.Action_Name = "Xem";
-            busLog.ThemLichSu(log);
+            //log.MChine = dtlog.Rows[0][1].ToString();
+            //log.UserID = dtlog.Rows[0][2].ToString();
+            //log.Module = this.Tag.ToString();
+            //log.Action_Name = "Xem";
+            //busLog.ThemLichSu(log);
 
+        }
+
+        private void btnDongY_Click(object sender, EventArgs e)
+        {
+            BUS_SYS_USER busform = new BUS_SYS_USER();
+         
+            int result = busform.CapNhatMatKhau(id,txtMatKhauCu.Text,txtMatKhauMoi.Text);
+            if (result == 1&&txtMatKhauMoi.Text==txtNhapLai.Text)
+                MessageBox.Show("Cập nhật thành công");
+            else
+                MessageBox.Show("Cập nhật thất bại");
         }
     }
 }
